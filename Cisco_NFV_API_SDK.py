f48dc7489ee750a2b4066dbbb73a0352ff7cbf3d
Kris Swanson <kris@swannynet.com>
2019-02-15 16:46:27 -0500

    bug fixes and sdk improvements

--- Cisco_NFV_API_SDK.py
+++ Cisco_NFV_API_SDK.py
@@ -49,8 +49,12 @@ class NFVIS_API_Calls:
         response = requests.delete(uri, verify=False, auth=HTTPBasicAuth(username,password),headers=header)
         return response.status_code, response
 
-    def post(username,password,uri,header,data):
+    def post(username,password,uri,header,xml_data='',json_data=''):
         '''gets the specified uri and returns: response code, json formatted response. '''
+        if xml_data != '':
+            data=xml_data
+        else:
+            data = json_data
         response = requests.post(uri, verify=False, auth=HTTPBasicAuth(username,password),headers=header,data=data)
         return response.status_code, response
 
@@ -60,28 +64,30 @@ class NFVIS_URNs:
     def __init__(self,url):
         self.url=url
 
-    def get(self,url):
+    def get(self,url,format='json'):
         '''returns appropriate REST GET uri and header given shorthand key'''
         rest_get_uri={'deployments':'%s/api/config/vm_lifecycle/tenants/tenant/admin/deployments'%url,
                       'platform-detail':"%s/api/operational/platform-detail"%url,
                       'networks':'%s/api/config/networks?deep'%url,
                       'bridges':'%s/api/config/bridges?deep'%url}
-        rest_get_json_header={"content-type": "application/vnd.yang.collection+json","Accept": "application/vnd.yang.data+json"}
-        rest_get_xml_header={}
-        return rest_get_uri[self],rest_get_json_header
-    def post(self,url,data=''):
-        '''returns appropriate REST POST uri and header given shorthand key and object to be posted'''
+        rest_get_header={'json':{"content-type": "application/vnd.yang.collection+json","Accept": "application/vnd.yang.data+json"},'xml':None}
+        return rest_get_uri[self],rest_get_header[format]
+    def post(self,url,format='json',vnf=None,bridge=None,network=None):
+        '''returns appropriate REST POST uri and header given shorthand key and object to be posted
+        url arguemnt needs to be in format: https://<ip or name of nfvis system>'''
         rest_post_uri={'bridges':"%s/api/config/bridges"%url,
                        'networks':"%s/api/config/networks"%url,
                        'deployments':"%s/api/config/vm_lifecycle/tenants/tenant/admin/deployments"%url}
-        rest_post_json_header={}
-        rest_post_xml_header={"content-type": "application/vnd.yang.data+xml","Accept": "application/vnd.yang.data+xml"}
-        return rest_post_uri[self],rest_post_xml_header
-    def delete(self,url,vnf='',bridge='',vswitch=''):
+        rest_post_header={'json':{'content-type': "application/vnd.yang.data+json", 'accept': "application/vnd.yang.data+json"},
+                          'xml':{"content-type": "application/vnd.yang.data+xml","Accept": "application/vnd.yang.data+xml"}}
+        rest_post_json_data={'bridges':"{\"bridge\": [{\"name\": \"%s\"}]}"%bridge,
+                             'networks':"{\"network\":[{\"name\":\"%s\",\"bridge\":\"%s\"}]}"%(network,bridge),
+                             'deployments':None}
+        return rest_post_uri[self],rest_post_header[format],rest_post_json_data[self]
+    def delete(self,url,format='json',vnf='',bridge='',network=''):
         '''returns appropriate REST DELETE uri and header given shorthand key and object to be deleted'''
         rest_delete_uri={'vnf':"%s/api/config/vm_lifecycle/tenants/tenant/admin/deployments/deployment/%s"%(url,vnf),
-                        'vswitch':'%s/api/config/networks/network/%s'%(url,vswitch),
+                        'network':'%s/api/config/networks/network/%s'%(url,network),
                         'bridge':'%s/api/config/bridges/bridge/%s'%(url,bridge)}
-        rest_delete_json_header={"content-type": "application/vnd.yang.collection+json","Accept": "application/vnd.yang.data+json"}
-        rest_delete_xml_header={}
-        return rest_delete_uri[self],rest_delete_json_header
+        rest_delete_header={'json':{"content-type": "application/vnd.yang.collection+json","Accept": "application/vnd.yang.data+json"},'xml':None}
+        return rest_delete_uri[self],rest_delete_header[format]
